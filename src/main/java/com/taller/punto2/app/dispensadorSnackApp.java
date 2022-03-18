83b04b527e566576d378125c937b614d5d97b9d0
cristianospina <cristian-david1998@live.com>
2022-03-18 17:57:27 -0500

    Taller

--- src/main/java/com/taller/punto2/app/dispensadorSnackApp.java
+++ src/main/java/com/taller/punto2/app/dispensadorSnackApp.java
@@ -12,22 +12,31 @@ public class dispensadorSnackApp {
         autoSnack.agregar("Mani salado", "5678", 800);
         autoSnack.agregar("Papa de limon", "9876", 2000);
         autoSnack.agregar("Galletas oreo", "5432", 500);
+        autoSnack.agregar("Boliquesos", "7913", 2000);
+        autoSnack.agregar("Turron de coco", "1379", 300);
+        autoSnack.agregar("Chocorramo", "8246", 3000);
+        autoSnack.agregar("Trident", "6428", 200);
+        autoSnack.agregar("Mani dulce", "8213", 1000);
+        autoSnack.agregar("Papa de pollo", "1328", 2000);
+        autoSnack.agregar("Chocolatina", "6398", 500);
+        autoSnack.agregar("Lecherita", "7412", 600);
 
-        autoSnack.quitarSnack("9876");
-        autoSnack.retirarSnackPorNombre("Doritos");
+        autoSnack.quitarSnack("6428");
+        autoSnack.retirarSnackPorNombre("Trident");
         autoSnack.retirarSnackPorCodigo("5678");
         autoSnack.snackOrdenadoPrecio();
-        autoSnack.retirarSnackPorNombre("Papas de limon");
+        autoSnack.retirarSnackPorNombre("Papa de limon");
         System.out.println("Lista Por Cantidad");
         autoSnack.snackOrdenadoCantidad();
+        autoSnack.cantidadSnack("8246");
         autoSnack.snackDisponibles();
         autoSnack.snackAgotados();
         System.out.println("Buscando snack");
         autoSnack.cantidadSnack("9876");
         System.out.println("Snack incrementado");
-        autoSnack.aumentarSnack("1234", 1);
-        autoSnack.aumentarSnack("5432", 15);
-        autoSnack.cantidadSnack("9876");
+        autoSnack.aumentarSnack("6398", 1);
+        autoSnack.aumentarSnack("1328", 15);
+
 
 
     }
